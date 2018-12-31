73d25c4845994948500386145f53a4d70df236a5
Evan Pratten <ewpratten@gmail.com>
2018-12-31 17:09:53 -0500

    basic logging

---
+++ AirLog/data.py
@@ -0,0 +1,17 @@
+import datetime
+
+
+def toHeadings(data):
+	output = []
+	for heading in data:
+		output.append(heading)
+	return output
+
+def write(heading, data):
+	line = ""
+	for point in data:
+		line += f"{data[point]},"
+	with open("./logs/"+ str(datetime.datetime.today().strftime('%Y-%m-%d')) + ".csv", "w") as f:
+		f.writelines(line[:-1]+"\n")
+		f.close()
+		print("Log updated.")
