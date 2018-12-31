4cb18ff43de59e7431f2792f0ded4a26ea5053b7
Evan Pratten <ewpratten@gmail.com>
2018-12-31 17:14:57 -0500

    removed old code

--- AirLog/__main__.py
+++ AirLog/__main__.py
@@ -1,6 +1,3 @@
-# from bs4 import BeautifulSoup
-# import requests
-
 import data as csv
 
 __version__ = "0.1"
@@ -22,36 +19,3 @@ while 0 < len(questions):
 
 headings = csv.toHeadings(data)
 csv.write(headings, data)
-
-
-
-
-# print("Callsign?")
-# callsign = input(">")
-
-# data = BeautifulSoup(requests.get(callsign_endpoint + callsign).text, "lxml")
-
-# names = []
-# loactions = []
-# licenses = []
-
-# location = data.find("b")
-# for i,tg in enumerate([str(tag) for tag in data.find_all()]):
-# 	if "<b>License Class:" in tg:
-# 		#licences
-# 		licenses.append(tg.split(">")[2])
-		
-# 		# name
-# 		if len([str(tag) for tag in data.find_all()][i-5].strip("<b>").strip("<br/>")) < 50:
-# 			names.append([str(tag) for tag in data.find_all()][i-5].strip("<b>").strip("<br/>"))
-		
-# 		# address
-# 		address = [str(tag) for tag in data.find_all()][i-4]
-# 		print(address)
-# 	# 	break
-		
-# 	# if tg[1] == "b":
-# 	# 	[print(tg)]
-		
-# print(f"Licenses: {licenses}")
-# print(f"Name: {names}")
