c0172388adc5240e9c0af93ed04eec506900982c
Evan Pratten <ewpratten@gmail.com>
2018-12-31 17:14:28 -0500

    added time tracking

--- AirLog/data.py
+++ AirLog/data.py
@@ -1,6 +1,5 @@
 import datetime
 
-
 def toHeadings(data):
 	output = []
 	for heading in data:
@@ -11,6 +10,7 @@ def write(heading, data):
 	line = ""
 	for point in data:
 		line += f"{data[point]},"
+	line += str(datetime.datetime.now().time()).split(".")[0][:-3]+","
 	with open("./logs/"+ str(datetime.datetime.today().strftime('%Y-%m-%d')) + ".csv", "w") as f:
 		f.writelines(line[:-1]+"\n")
 		f.close()
