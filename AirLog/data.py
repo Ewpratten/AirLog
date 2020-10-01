4e986223d7b5eafe437f305ba0d00de3cfc08a3b
Ross Wardrup <minorsecond@gmail.com>
2020-10-01 05:47:37 -0500

    Remove call to f.read()

--- AirLog/data.py
+++ AirLog/data.py
@@ -14,7 +14,6 @@ def write(heading, data):
 		line += f"{data[point]},"
 	line += str(datetime.datetime.now().time()).split(".")[0][:-3]+","
 	with open("../logs/" + str(datetime.datetime.today().strftime('%Y-%m-%d')) + ".csv", "w") as f:
-		file = f.read()
-		f.writelines(str(file) + line[:-1]+"\n")
+		f.writelines(line[:-1]+"\n")
 		f.close()
 		print("Log updated.")
