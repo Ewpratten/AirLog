d8c686ee061129e17bd7bb54175e6994d56ce908
Ross Wardrup <minorsecond@gmail.com>
2020-10-01 05:45:29 -0500

    Correct relative path to log directory

--- AirLog/data.py
+++ AirLog/data.py
@@ -1,17 +1,19 @@
 import datetime
 
+
 def toHeadings(data):
 	output = []
 	for heading in data:
 		output.append(heading)
 	return output
 
+
 def write(heading, data):
 	line = ""
 	for point in data:
 		line += f"{data[point]},"
 	line += str(datetime.datetime.now().time()).split(".")[0][:-3]+","
-	with open("./logs/"+ str(datetime.datetime.today().strftime('%Y-%m-%d')) + ".csv", "w") as f:
+	with open("../logs/" + str(datetime.datetime.today().strftime('%Y-%m-%d')) + ".csv", "w") as f:
 		file = f.read()
 		f.writelines(str(file) + line[:-1]+"\n")
 		f.close()
